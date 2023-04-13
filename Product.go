0a1feef558fb37a8adac1d9da2c7ef10da85645e
Agil Adi Saputro <agiladisaputro@gmail.com>
2023-04-13 12:04:47 +0700

    intiate gorm. initiate struct/table for migration

---
+++ Product.go
@@ -0,0 +1,14 @@
+package entity
+
+import (
+	"time"
+)
+
+type Product struct {
+	ID uint `gorm:"primaryKey"`
+	Name string `gorm:"not null;type:varchar(200)"`
+	Brand string `gorm:"not null;type:varchar(200)"`
+	UserID uint
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
---
+++ entity/User.go
@@ -0,0 +1,15 @@
+package entity
+
+import (
+	"time"
+)
+
+type User struct {
+	ID        uint `gorm:"prmaryKey"`
+	Email     string `gorm:not null;unique;type:varchar(20)`
+	Products []Product
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+
